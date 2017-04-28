0cb99c71c14fac28f4a030b360694da4b4934aae
OrghoN <anoronyo@gmail.com>
2017-04-28 01:10:45 -0500

    added extra zsh completions

--- userConf.zsh
+++ userConf.zsh
@@ -40,7 +40,8 @@ COMPLETION_WAITING_DOTS="true"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(colored-man-pages colorize cp git github node npm pip dirpersist zsh-autosuggestions)
+plugins=(colored-man-pages colorize cp git github node npm pip dirpersist zsh-autosuggestions zsh-completions)
+autoload -U compinit && compinit
 
 # User configuration
 
