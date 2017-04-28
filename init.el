68a1d681edbbe67f4877731fccbfb50e064e2769
OrghoN <anoronyo@gmail.com>
2017-04-28 02:23:27 -0500

    added autocomplete emacs

--- init.el
+++ init.el
@@ -11,6 +11,8 @@
 (setq linum-format "%4d \u2502 ")
 (global-linum-mode t)
 
+(ac-config-default)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -19,7 +21,7 @@
  '(custom-safe-themes
    (quote
     ("a1289424bbc0e9f9877aa2c9a03c7dfd2835ea51d8781a0bf9e2415101f70a7e" "08b8807d23c290c840bbb14614a83878529359eaba1805618b3be7d61b0b0a32" default)))
- '(package-selected-packages (quote (atom-dark-theme atom-one-dark-theme))))
+ '(package-selected-packages (quote (auto-complete atom-dark-theme atom-one-dark-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
