9b126ab4e7e15f64a97ae86da775daa36983a07b
OrghoN <anoronyo@gmail.com>
2017-05-06 21:23:25 -0500

    incorporated syntax hilighting into dotFiles

--- init.el
+++ init.el
@@ -23,7 +23,9 @@
  '(custom-safe-themes
    (quote
     ("a1289424bbc0e9f9877aa2c9a03c7dfd2835ea51d8781a0bf9e2415101f70a7e" "08b8807d23c290c840bbb14614a83878529359eaba1805618b3be7d61b0b0a32" default)))
- '(package-selected-packages (quote (auto-complete atom-dark-theme atom-one-dark-theme))))
+ '(package-selected-packages
+   (quote
+    (## auto-complete atom-dark-theme atom-one-dark-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
