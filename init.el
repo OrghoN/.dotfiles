74844d140ab5933d1c3c825b8ec87d937e36bfcd
OrghoN <anoronyo@gmail.com>
2025-02-03 13:22:34 -0600

    Updated emacs startup
    Runs emacspeak
    aster
    aster doesnt work
    
    loads slime
    
    makes things case insensetive for completion
    
    sets speech rate to 350
    
    sets theme to atom-dark
    
    has electric brackets
    
    loads melpa

--- init.el
+++ init.el
@@ -1,19 +1,34 @@
-(require 'package) ;; You might already have this line
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize) ;; You might already have this line
+;; Setting up emacs to use emacspeak
+;; emacspeak has been installed from source in /opt/emacspeak
+(load-file "/opt/emacspeak/lisp/emacspeak-setup.el")
 
+;;  Setting up emacs to use ASTER
+(add-to-list 'load-path "/opt/emacspeak/aster-math/ui")
+
+;; setting up slime with sbcl
+(setq inferior-lisp-program "/usr/bin/sbcl")
+
+;; Setting up melpa
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+;; Case-Insensetive completion
+(setq completion-ignore-case t)
+(setq read-file-name-completion-ignore-case t)
+(setq read-buffer-completion-ignore-case t)
+
+;; auto close bracket insertion
+(electric-pair-mode 1)
+
+;; Set speech rate to 350
+(dtk-set-rate 350 (universal-argument))
+
+;;Load Theme
 (load-theme 'atom-dark t)
-
-(setq linum-format "%4d \u2502 ")
-(global-linum-mode t)
-
-(ac-config-default)
-
-(electric-pair-mode)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -21,11 +36,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("a1289424bbc0e9f9877aa2c9a03c7dfd2835ea51d8781a0bf9e2415101f70a7e" "08b8807d23c290c840bbb14614a83878529359eaba1805618b3be7d61b0b0a32" default)))
- '(package-selected-packages
-   (quote
-    (## auto-complete atom-dark-theme atom-one-dark-theme))))
+   '("4eb9462a8fff9153bfe88a9ef53aa043aec8b79c5298d2873e887e0c3a8b03de" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
